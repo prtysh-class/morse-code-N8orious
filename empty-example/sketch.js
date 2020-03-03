c6c1f38f00b773e26b2a684ad4e9c83f450b7144
N8orious <keshavraj6301@gmail.com>
2020-03-04 01:04:16 +0530

    second

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,40 +1,101 @@
+let greeting;
+let s;
+var input;
+var mc1 = "";
 function setup() {
-  createCanvas(700, 500);
+  createCanvas(400, 400);
+  input = createInput();
+  input.position(20, 65);
+  input.changed(newText);
+  
+
+  
+  
+   greeting = createElement('h2', 'Enter Text');
+  greeting.position(20, 5);
 }
 
 function draw() {
-  let z = random(255);
-  noStroke();
-  background(0);
-  
-  beginShape();
-  
-  let spacing = map(mouseX, 0, width, 40, 550);
-  if(spacing  > 50){
-  fill(153, 51, 255);}
-  
-  if(spacing > 75){
-    fill(255, 255, 0);}
-  
-  if(spacing > 150){
-    fill(255, 0, 0);}
-  
-  if(spacing > 225){
-    fill(255, 0, 255);}
-  
-  if(spacing > 300){
-    fill(51, 255, 51);}
-  
-  if(spacing > 375){
-    fill(102, 255, 255);}
-  
-  if(spacing > 450){
-    fill(0);}
-  
-  
+  background(220);
+ 
+}
+
+function morseEncode(x)  
+    { 
+      
+        { 
+            case 'a': 
+                return ".-"; 
+            case 'b': 
+                return "-..."; 
+            case 'c': 
+                return "-.-."; 
+            case 'd': 
+                return "-.."; 
+            case 'e': 
+                return "."; 
+            case 'f': 
+                return "..-."; 
+            case 'g': 
+                return "--."; 
+            case 'h': 
+                return "...."; 
+            case 'i': 
+                return ".."; 
+            case 'j': 
+                return ".---"; 
+            case 'k': 
+                return "-.-"; 
+            case 'l': 
+                return ".-.."; 
+            case 'm': 
+                return "--"; 
+            case 'n': 
+                return "-."; 
+            case 'o': 
+                return "---"; 
+            case 'p': 
+                return ".--."; 
+            case 'q': 
+                return "--.-"; 
+            case 'r': 
+                return ".-."; 
+            case 's': 
+                return "..."; 
+            case 't': 
+                return "-"; 
+            case 'u': 
+                return "..-"; 
+            case 'v': 
+                return "...-"; 
+            case 'w': 
+                return ".--"; 
+            case 'x': 
+                return "-..-"; 
+            case 'y': 
+                return "-.--"; 
+            // for space 
+            case 'z': 
+                return "--.."; 
+        } 
+        return ""; 
+    }
+
   
   
 
-  ellipse(250, 250, spacing);
-    endShape(CLOSE);
+function newText()
+  {
+    console.log(input.value());
+    s = input.value();
+    morsecode(s);
+  }
+
+function morsecode(s)
+{
+  for (var i = 0;i<textWidth(s); i++) 
+            console.log( 'size  ' + textWidth(s));
+  print(morseEncode((i)));
+  text(40, 40, 40);
 }
+ 
